285cc64539dc297cf831f312f3d2122d27954a31
Tyler Moody <tyler@Tylers-MacBook-Pro.local>
2018-05-25 12:20:40 -0500

    fixing permissions issue with fly cli

--- configure-pipelines.sh
+++ configure-pipelines.sh
@@ -6,6 +6,10 @@ wget --no-check-certificate https://github.com/concourse/concourse/releases/down
 
 wget --no-check-certificate https://github.com/concourse/concourse/releases/download/v3.13.0/fly_linux_amd64
 
+chmod +x fly_linux_amd64
+
+./fly_linux_amd64 login -t http://54.209.196.185:8080
+
 for file in pipeline-config/*; do
     ./fly_linux_amd64 -t concourse -p $(basename "$file") -c file
 done
