5088f450d0117305f0136d1f81c08d6eac17b9f8
Tyler Moody <tyler@Tylers-MacBook-Pro.local>
2018-05-25 12:19:00 -0500

    adding no-check-certificate

--- configure-pipelines.sh
+++ configure-pipelines.sh
@@ -2,9 +2,9 @@
 
 set -e -x
 
-wget https://github.com/concourse/concourse/releases/download/v3.13.0/concourse_linux_amd64
+wget --no-check-certificate https://github.com/concourse/concourse/releases/download/v3.13.0/concourse_linux_amd64
 
-wget https://github.com/concourse/concourse/releases/download/v3.13.0/fly_linux_amd64
+wget --no-check-certificate https://github.com/concourse/concourse/releases/download/v3.13.0/fly_linux_amd64
 
 for file in pipeline-config/*; do
     ./fly_linux_amd64 -t concourse -p $(basename "$file") -c file
