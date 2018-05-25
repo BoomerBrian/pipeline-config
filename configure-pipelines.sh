91b24d0fb54c8a862fab392b5fdf7312fffddf71
Tyler Moody <tyler@Tylers-MacBook-Pro.local>
2018-05-25 12:41:24 -0500

    GOIN FOR IT

--- configure-pipelines.sh
+++ configure-pipelines.sh
@@ -9,6 +9,6 @@ chmod +x fly_linux_amd64
 ./fly_linux_amd64 --target concourse login --concourse-url http://54.209.196.185:8080/
 
 for file in pipeline-config/*-config.yml; do
-    filename="${filename%.*}"
+    filename="$(basename ${file%.*})"
     ./fly_linux_amd64 -t concourse set-pipeline -n -p $filename -c pipeline-config/$(basename "$file")
 done
