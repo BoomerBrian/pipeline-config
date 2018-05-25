fd6857a8c62ae7d76f8b929b592f643c20b025fd
Tyler Moody <tyler@Tylers-MacBook-Pro.local>
2018-05-25 12:23:39 -0500

    squashing unneccessary fly

--- configure-pipelines.sh
+++ configure-pipelines.sh
@@ -6,7 +6,7 @@ wget --no-check-certificate https://github.com/concourse/concourse/releases/down
 
 chmod +x fly_linux_amd64
 
-./fly_linux_amd64 fly --target concourse login --concourse-url http://54.209.196.185:8080/
+./fly_linux_amd64 --target concourse login --concourse-url http://54.209.196.185:8080/
 
 for file in pipeline-config/*; do
     ./fly_linux_amd64 -t concourse -p $(basename "$file") -c file
